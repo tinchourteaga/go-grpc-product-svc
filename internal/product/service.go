a95a828bda7098c9e37fa31d22b93e647386b1e9
tinchourteaga <38136900+tinchourteaga@users.noreply.github.com>
2023-12-20 17:22:31 -0300

    service layer

--- internal/product/service.go
+++ internal/product/service.go
@@ -1,1 +1,47 @@
 package product
+
+import (
+	"context"
+
+	"github.com/tinchourteaga/go-grpc-product-svc/internal/models"
+)
+
+type Service interface {
+	Create(context.Context, models.Product) (int64, error)
+	FindOne(context.Context, int64) (models.Product, error)
+	DecreaseStock(context.Context, models.StockDecreaseLog) error
+}
+
+type service struct {
+	repository Repository
+}
+
+func NewService(repo Repository) Service {
+	return &service{
+		repository: repo,
+	}
+}
+
+func (s *service) Create(ctx context.Context, product models.Product) (int64, error) {
+	id, err := s.repository.Create(ctx, product)
+	if err != nil {
+		return 0, err
+	}
+	return id, nil
+}
+
+func (s *service) FindOne(ctx context.Context, id int64) (models.Product, error) {
+	product, err := s.repository.FindOne(ctx, id)
+	if err != nil {
+		return models.Product{}, err
+	}
+	return product, nil
+}
+
+func (s *service) DecreaseStock(ctx context.Context, stkDecreaseLog models.StockDecreaseLog) error {
+	err := s.repository.DecreaseStock(ctx, stkDecreaseLog)
+	if err != nil {
+		return err
+	}
+	return nil
+}
