296cf2a8046fd71caa3edbae9f7b4c060c8a2fbd
tinchourteaga <38136900+tinchourteaga@users.noreply.github.com>
2023-12-20 16:16:19 -0300

    product repository layer

--- internal/product/repository.go
+++ internal/product/repository.go
@@ -1,1 +1,102 @@
 package product
+
+import (
+	"context"
+	"errors"
+	"strconv"
+
+	"github.com/rs/zerolog/log"
+	"github.com/tinchourteaga/go-grpc-product-svc/internal/models"
+	"github.com/tinchourteaga/go-grpc-product-svc/pkg/db"
+	"gorm.io/gorm"
+)
+
+type Repository interface {
+	Create(context.Context, models.Product) error
+	FindOne(string) (models.Product, error)
+	DecreaseStock(context.Context, models.StockDecreaseLog) error
+	IncreaseStock(context.Context, models.Product) error
+	Exists(context.Context, models.Product) bool
+}
+
+type repository struct {
+	con db.Connector
+}
+
+func NewRepository(con db.Connector) Repository {
+	return &repository{
+		con: con,
+	}
+}
+
+func (r *repository) Create(ctx context.Context, product models.Product) error {
+	exists := r.Exists(ctx, product)
+	if exists {
+		err := r.IncreaseStock(ctx, product)
+		if err != nil {
+			return err
+		}
+		return nil
+	}
+
+	r.con.DB.Create(&product)
+	return nil
+}
+
+func (r *repository) FindOne(id string) (models.Product, error) {
+	product := models.Product{}
+	result := r.con.DB.First(&product, id)
+	if errors.Is(result.Error, gorm.ErrRecordNotFound) {
+		return models.Product{}, gorm.ErrRecordNotFound
+	}
+
+	return product, nil
+}
+
+func (r *repository) DecreaseStock(ctx context.Context, stkDecreaseLog models.StockDecreaseLog) error {
+	dbProduct := models.Product{}
+	result := r.con.DB.First(&dbProduct, stkDecreaseLog.Id)
+	if errors.Is(result.Error, gorm.ErrRecordNotFound) {
+		return gorm.ErrRecordNotFound
+	}
+
+	if dbProduct.Stock < 1 {
+		return errors.New("no stock for product: " + dbProduct.Name)
+	}
+
+	dbStkDecreaseLog := models.StockDecreaseLog{}
+	result = r.con.DB.Where(&models.StockDecreaseLog{OrderId: stkDecreaseLog.OrderId}).First(&dbStkDecreaseLog)
+	if result.Error == nil {
+		return errors.New("stock already decreased")
+	}
+
+	dbProduct.Stock = dbProduct.Stock - 1
+	r.con.DB.Save(&dbProduct)
+
+	dbStkDecreaseLog.OrderId = stkDecreaseLog.OrderId
+	dbStkDecreaseLog.ProductRefer = dbProduct.Id
+
+	r.con.DB.Create(&dbStkDecreaseLog)
+
+	log.Info().Msg("stock decreased for product " + dbProduct.Name)
+	return nil
+}
+
+func (r *repository) IncreaseStock(ctx context.Context, product models.Product) error {
+	dbProduct := models.Product{}
+	result := r.con.DB.Where(&models.Product{Name: product.Name, Price: product.Price}).First(&dbProduct)
+	if errors.Is(result.Error, gorm.ErrRecordNotFound) {
+		return gorm.ErrRecordNotFound
+	}
+
+	dbProduct.Stock = dbProduct.Stock + product.Stock
+	r.con.DB.Save(&dbProduct)
+
+	log.Info().Msg("stock increased by " + strconv.Itoa(int(product.Stock)) + " for product " + product.Name)
+	return nil
+}
+
+func (r *repository) Exists(ctx context.Context, product models.Product) bool {
+	result := r.con.DB.Where(&models.Product{Name: product.Name, Price: product.Price}).First(&models.Product{})
+	return result.Error == nil
+}
